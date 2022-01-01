ef39f8b99d3bffeb8e5882746d7ed6829b31c133
Jose Angarita <amjose09@gmail.com>
2022-01-01 13:16:02 -0500

    Routeo basico listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Login } from './pages/auth/Login'
+import { Register } from './pages/auth/Register'
+import { Index } from './pages/Index'
 import './style/style.scss'
 
 const App = () => {
     return (
-        <div className="text-3xl w-screen h-screen flex items-center justify-center">
-            Main page
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Index/>}/>
+                <Route path="/auth/login" element={<Login/>}/>
+                <Route path="/auth/register" element={<Register/>}/>
+            </Routes>
+        </Router>
     )
 }
 
